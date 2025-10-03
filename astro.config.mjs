ab98edbd4bde6d4e58e3379a9a2ac226865e202c
antoniorb1913 <136896845+antoniorb1913@users.noreply.github.com>
2025-10-03 19:34:51 +0200

    todo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,4 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
-site: 'https://astronaut.github.io',
+site: 'https://antoniorb1913.github.io',
